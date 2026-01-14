b232db7e9ed211fe7b7137df72f0938bfad857b4
znoland <znoland@carthage.edu>
2026-01-14 15:13:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,69 @@
 #include <iostream>
-#include "Greeter.h"
+#include <vector>
 using namespace std;
 
-int main() 
-{
-    string name = "World";
-    
-    Greeter greeter(name);
-    cout<<greeter.getGreeting()<<endl;
+vector<int> getNumbers();
+int findMostFrequentNumber(vector<int> nums);
+int numberGreaterThanAverage(vector<int> nums);
+int greatestCommonDivisor(vector<int> nums);
+bool isSorted(vector<int> nums);
 
-    greeter.setWelcomeMessage("Hola");
-    cout<<greeter.getGreeting()<<endl;
+int main()
+{
+    //get the numbers from the user
+    vector<int> numbers = getNumbers();
+
+    //process the numbers and print results
+    cout<<"Most frequent number: "<<findMostFrequentNumber(numbers)<<endl;
+    
+    cout<<"There are "<<numberGreaterThanAverage(numbers)<<" numbers greater than the average."<<endl;
+    
+    cout<<"The greatest common divisor is: "<<greatestCommonDivisor(numbers)<<endl;
+    
+    if(isSorted(numbers))
+    {
+        cout<<"The numbers are sorted."<<endl;
+    }
+    else
+    {
+        cout<<"The numbers are not sorted."<<endl;
+    }
 
     return 0;
+}
+//--
+vector<int> getNumbers()
+{
+    vector<int> nums;
+    int num;
+    cout << "Enter numbers (0 to stop): ";
+    while (cin >> num && num != 0)
+    {
+        nums.push_back(num);
+    }
+    return nums;
+}
+//--
+int findMostFrequentNumber(vector<int> nums)
+{
+    // TODO: Student 1
+    return -1;
+}
+//--
+int numberGreaterThanAverage(vector<int> nums)
+{
+    // TODO: Student 2
+    return 0;
+}
+//--
+int greatestCommonDivisor(vector<int> nums)
+{
+    // TODO: Student 3
+    return 1;
+}
+//--
+bool isSorted(vector<int> nums)
+{
+    // TODO: Student 4
+    return false;
 }
